8a09a6a56094345ff3beb039532ff12dbccdd98e
TMuthanna <Muthannatalvar@gmail.com>
2025-11-25 10:28:44 +0100

    Add files via upload

---
+++ AfterVacation/src/test/java/MavenProject/AfterVacation/AppTest.java
@@ -0,0 +1,19 @@
+package MavenProject.AfterVacation;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
